b1862fb60a57356eedee09d2e894b91def33762a
Seong Kim <runmath@gmail.com>
2023-05-25 23:54:08 -0600

    test done

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,35 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
-  return (
-    <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
-  );
+function About(props) {
+
+  console.log(props);
+
+  if (props.bio === undefined || props.bio === "") {
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        {/* add your <Links /> component here */}
+        <div>
+          <Links github={props.links.github} linkedin={props.links.linkedin} />
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        <p>{props.bio}</p>
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        {/* add your <Links /> component here */}
+        <div>
+          <Links github={props.links.github} linkedin={props.links.linkedin} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default About;
